0516061bd6aff135af6a9a28c3c74f38a60b8bc3
Wally Young <wally.young@daqri.com>
2015-11-20 16:54:00 -0800

    Removing logging.

--- src/Unity/Assets/ARToolKit5-Unity/Scripts/ARTrackedObject.cs
+++ src/Unity/Assets/ARToolKit5-Unity/Scripts/ARTrackedObject.cs
@@ -52,7 +52,6 @@ public class ARTrackedObject : AAREventReciever {
 			return;
 		}
 		for (int i = 0; i < transform.childCount; ++i) {
-			Debug.LogError(transform.GetChild(i).name);
 			transform.GetChild(i).gameObject.SetActive(true);
 		}
 	}
